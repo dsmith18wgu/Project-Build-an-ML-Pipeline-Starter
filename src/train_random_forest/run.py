ccdb5f722724f0b150aaf5ce5c25dbd47f3d7a1e
dsmith18wgu <dsmith18@wgu.edu>
2025-08-31 20:01:18 -0500

    updating the run file for random forest

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -70,11 +70,7 @@ def go(args):
 
     # Then fit it to the X_train, y_train data
     logger.info("Fitting")
-
-    ######################################
-    # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
-    ######################################
+    sk_pipe.fit(X_train, y_train)
 
     # Compute r2 and MAE
     logger.info("Scoring")
@@ -95,8 +91,13 @@ def go(args):
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
+
+    export_path= "random_forest_dir"
+
     mlflow.sklearn.save_model(
-        # YOUR CODE HERE
+        sk_pipe,
+        export_path,
+        serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -119,8 +120,8 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
-    # YOUR CODE HERE
-    ######################################
+   
+    run.summary['mae'] = mean_absolute_error
 
     # Upload to W&B the feture importance visualization
     run.log(
@@ -162,7 +163,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        # YOUR CODE HERE
+        SimpleImputer(strategy="most_frequent"), OneHotEncoder()
     )
     ######################################
 
@@ -225,7 +226,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-        # YOUR CODE HERE
+        ("preprocessor", preprocessor),
+        ("random_forest", random_forest)
         ]
     )
 
