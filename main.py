615e64ac405ac1c80300085160f09f8f764c98cb
dsmith18wgu <dsmith18@wgu.edu>
2025-08-31 20:10:15 -0500

    typo in path

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def go(config: DictConfig):
                     "random_seed": config['modeling']['random_seed'],
                     "stratify_by":config['modeling']['stratify_by'],
                     "rf_config":rf_config,
-                    "max_tfdif_features":config['modeling']['max_tfdif_features'],
+                    "max_tfidf_features":config['modeling']['max_tfidf_features'],
                     "output_artifact":"random_forest_export.csv"
                 },
             )
