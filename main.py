04d29fc85f8d6231ef7529417527d320294a1bee
dsmith18wgu <dsmith18@wgu.edu>
2025-08-31 20:21:51 -0500

    fix output artifact name

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def go(config: DictConfig):
                     "stratify_by":config['modeling']['stratify_by'],
                     "rf_config":rf_config,
                     "max_tfidf_features":config['modeling']['max_tfidf_features'],
-                    "output_artifact":"random_forest_export.csv"
+                    "output_artifact":"random_forest_export"
                 },
             )
 
